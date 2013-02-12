66c52cf2848d8f2d3007952c2a8d4d1dba9a6ec9
pdarkness <kntur11@ru.is>
2013-02-12 21:17:08 +0000

    Parserinn

--- src/Parser.java
+++ src/Parser.java
@@ -1,3 +1,5 @@
+import java.util.regex.PatternSyntaxException;
+
 /*
 Implement the class Parser, the syntax analyzer (parser).
 This should be a top-down recursive-descent parser for the grammar G above,
@@ -7,15 +9,54 @@ corresponding to the given expression, written to standard output (stdout).
 If the expression is not in the language (or if an ERROR token is returned by the Lexer)
 then the parser should output the error message "Syntax error!"
 (at the point when the error is recognized) and immediately quit.
- */
+*/
 public class Parser {
     private Lexer lexer;
     private Token token;
     public Parser(Lexer lexer )
     {
         this.lexer=lexer;
+        token = lexer.nextToken();
     }
-    public void parse() {
 
+    public void expr() {
+        System.out.print(token.gettCode());
+        while ( token.gettCode() == TokenCode.PLUS ) {
+            token = lexer.nextToken();
+            term();
+        }
+    }
+
+    public void term() {
+        factor(); /* parses the first factor */
+        while ( token.gettCode() == TokenCode.MULT) {
+            token = lexer.nextToken();
+            factor();
+        }
+    }
+
+    public void factor() {
+        /* Ákvarða hvora málregluna eigi að nota */
+        if (token.gettCode() == TokenCode.INT)
+            token = lexer.nextToken(); /* get the next token */
+        else if (token.gettCode() == TokenCode.LPAREN)
+        {
+            token = lexer.nextToken();
+            expr();
+            if (token.gettCode() == TokenCode.RPAREN)
+                token = lexer.nextToken();
+            else
+                throw new PatternSyntaxException("Syntax Error","Token not correct",token.hashCode());
+        }
+        else error(); /* neither and id nor a left parenthesis */
+    }
+
+    public void error() {
+        System.err.print("Cheerios error");
+        throw new StackOverflowError("Error fallid");
+    }
+
+    public void parse() {
+        expr();
     }
 }
