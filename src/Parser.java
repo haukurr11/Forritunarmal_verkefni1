743d519c7cf37eb62aa41c9ba9137c83354bc79b
HaukurLap <hauxir@gmail.com>
2013-02-14 00:37:19 +0000

    Mooshak komið í 1 error

--- src/Parser.java
+++ src/Parser.java
@@ -9,67 +9,66 @@ corresponding to the given expression, written to standard output (stdout).
 If the expression is not in the language (or if an ERROR token is returned by the Lexer)
 then the parser should output the error message "Syntax error!"
 (at the point when the error is recognized) and immediately quit.
+WTF
 */
 public class Parser {
     private Lexer lexer;
     private Token token;
-    Stack<Token> operators;
-    Stack<Token> nums;
-    int currentFactor;
-    int lastFactor;
-    boolean tala_before = false;
     Node root;
     Node current;
     private class Node {
         Node parent;
-        public LinkedList<Token> operators;
-        public LinkedList<Token> nums;
+        public List<Token> operators;
+        public List<Token> nums;
         public List<Node> children;
         public Node(Node parent)
         {
-            operators = new LinkedList<Token>();
-            nums = new LinkedList<Token>();
+            operators = new ArrayList<Token>();
+            nums = new ArrayList<Token>();
             children = new ArrayList<Node>();
             this.parent=parent;
         }
     }
     public Parser(Lexer lexer )
     {
-        currentFactor = 0;
-        lastFactor = 0;
         this.lexer=lexer;
         token = this.lexer.nextToken();
-        operators= new Stack<Token>();
-        nums = new Stack<Token>();
         root = new Node(null);
         current = root;
     }
 
-    public void expr() {
+    private void expr() {
         Node newNode = new Node(current);
         current.children.add(newNode);
+        current = newNode;
         term();
         while ( token.gettCode() == TokenCode.PLUS ) {
             current.operators.add(token);
             token = lexer.nextToken();
             expr();
+            return;
         }
+        current=current.parent;
     }
 
-    public void term() {
+    private void term() {
         Node newNode = new Node(current);
         current.children.add(newNode);
         current = newNode;
         factor(); /* parses the first factor */
+        if(token.gettCode() == TokenCode.ERROR)
+            error();
+        if(token.gettCode() == TokenCode.END)
+            print();
         while ( token.gettCode() == TokenCode.MULT) {
-            current.operators.push(token);
+            current.operators.add(token);
             token = lexer.nextToken();
             term();
         }
-        current = current.parent;
+        current=current.parent;
     }
 
-    public void factor() {
+    private void factor() {
         /* Decide what rule to use */
         if (token.gettCode() == TokenCode.INT)
         {
@@ -85,35 +84,26 @@ public class Parser {
             current = newNode;
             token = lexer.nextToken();
             expr();
-            Token tempTok = lexer.nextToken();
             if (token.gettCode() == TokenCode.RPAREN) {
                     current = newNode.parent;
             }
-            token = tempTok;
+            token = lexer.nextToken();
         }
-
-
-        else error(); /* neither and id nor a left parenthesis */
+        else
+            error();
     }
 
-    public void error() {
+    private void error() {
+        print();
         System.out.println("Syntax error!");
         System.exit(1);
     }
 
-    public void end() {
-        System.out.println("End!");
-
-    }
-
-    public void print(Node node) {
+    private void print(Node node) {
         if(node == null) return;
-        //System.out.println("----------BEGIN PRINT");
-
         for(Token it : node.nums)
         {
             System.out.println("PUSH " + it.getLexeme());
-
         }
         for(Node ch : node.children)
         {
@@ -127,14 +117,13 @@ public class Parser {
             else if(it.gettCode()==TokenCode.PLUS)
                 System.out.println("ADD");
         }
-
-        //System.out.println("------------END PRINT");
     }
 
-    public void parse() {
-        expr();
-       // end();
+    private void print() {
         print(root);
         System.out.println("PRINT");
+    }
+    public void parse() {
+        expr();
     }
 }
